e7d9aeac168337f409683e3efb0ae70054e92b8d
Nicot Emeric <emeric.nicot@sfr.fr>
2019-01-21 00:44:10 +0100

    Main Class

--- GNURadio_UI/src/application/Main.java
+++ GNURadio_UI/src/application/Main.java
@@ -1,25 +1,65 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	 private Stage mainStage;
+	 private BorderPane mainContainer;
+
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		mainStage = primaryStage;
+	     //Ca ne vous rappelle pas une JFrame ?
+		mainStage.setTitle("COMRadio UI");
+	     //Nous allons utiliser nos fichier FXML dans ces deux méthodes
+	     initialisationConteneurPrincipal();
+	     initialisationContenu();
 	}
 	
+	private void initialisationConteneurPrincipal() {
+        //On créé un chargeur de FXML
+        FXMLLoader loader = new FXMLLoader();
+        //On lui spécifie le chemin relatif à notre classe
+        //du fichier FXML a charger : dans le sous-dossier vie
+        loader.setLocation(Main.class.getResource("view/MainContainer.fxml"));
+        try {
+            //Le chargement nous donne notre conteneur
+        	mainContainer = (BorderPane) loader.load();
+            //On définit une scène principale avec notre conteneur
+            Scene scene = new Scene(mainContainer);
+            //Que nous affectons à notre Stage
+            mainStage.setScene(scene);
+            //Pour l'afficher
+            mainStage.show();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    private void initialisationContenu() {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("view/UIView.fxml"));
+        try {
+            //Nous récupérons notre conteneur qui contiendra les données
+            //Pour rappel, c'est un AnchorPane...
+            AnchorPane appContainer = (AnchorPane) loader.load();
+            //Qui nous ajoutons à notre conteneur principal
+            //Au centre, puisque'il s'agit d'un BorderPane
+            mainContainer.setCenter(appContainer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 	public static void main(String[] args) {
 		launch(args);
 	}
