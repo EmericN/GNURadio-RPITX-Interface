1f86f610db4be29d14070bf5d6a24637603c7741
Nicot Emeric <emeric.nicot@sfr.fr>
2019-01-25 00:45:14 +0100

    Voice call test

--- GNURadio_UI/src/application/model/SocketListener.java
+++ GNURadio_UI/src/application/model/SocketListener.java
@@ -5,8 +5,6 @@ public interface SocketListener {
     public void onMessage(String line);
     //public void voiceIN(float sampleRate, int sampleSizeInBits,int channels,boolean signed, boolean bigEndian);
     //public void voiceOUT(float sampleRate, int sampleSizeInBits,int channels,boolean signed, boolean bigEndian);
-    public void voiceOverNetwork(float sampleRate, int sampleSizeInBits, int channels, boolean signed,
-    		boolean bigEndian);
     public void serverStatusClient(boolean isClientConnected);
     public void clientStatusClient(boolean isClientConnected);
     public void connectivityStatus(boolean isConnectivity);
